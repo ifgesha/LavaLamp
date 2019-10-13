c29e5d2044ad72289aa3b056de1b571df17bd325
Евгений <mirk@stationnet.ru>
2019-10-14 00:37:55 +0300

    Lamp udp

---
+++ app/src/main/java/com/mirk/gyverlamp/AppCallback.java
@@ -0,0 +1,5 @@
+package com.mirk.gyverlamp;
+
+public interface AppCallback {
+    public void dataHandler(String str);
+}
--- app/src/main/java/com/mirk/gyverlamp/MainActivity.java
+++ app/src/main/java/com/mirk/gyverlamp/MainActivity.java
@@ -4,17 +4,31 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity  implements AppCallback{
+
+
+    public boolean connectTitle = false;
+    public boolean powerOn = false;
+
+    private int SoTimeout = 10000;
+    private int responseSize = 80000;
+
+
+    ImageButton imageButton;
+
+    UDP_Client udpClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +37,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //FloatingActionButton fab = findViewById(R.id.fab);
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+
+        udpClient = new UDP_Client(this);
+        udpClient.setIpPort("192.168.0.104", 8888);
+
+        // Получим текущее состояние
+        udpClient.sendCommand("GET", true);
+        new Thread(udpClient).start();
+
+
+
+        // Включение лампы
+        imageButton = findViewById(R.id.buttonPower);
+
+        imageButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+
+                Log.i("lamp", "Click ");
+
+                // меняем изображение на кнопке 
+                if (powerOn){
+                    imageButton.setColorFilter( getResources().getColor(R.color.colorNoConnect));
+                    udpClient.sendCommand("P_OFF");
+                    new Thread(udpClient).start();
+                } else {
+                    imageButton.setColorFilter(getResources().getColor(R.color.colorPowerOn));
+                    udpClient.sendCommand("P_ON");
+                    new Thread(udpClient).start();
+                }
+                powerOn = !powerOn;
             }
-        });*/
-
-        // Нет подключения
-        setTitle(getString(R.string.app_name) + " - " + getString(R.string.no_connect) );
-        getSupportActionBar().setBackgroundDrawable(
-                new ColorDrawable(
-                        getResources().getColor(R.color.colorNoConnect)
-                )
-        );
-
-
-
-
+        });
 
 
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,6 +93,10 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        udpClient.sendCommand("GET", true);
+        new Thread(udpClient).start();
+
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_sleep) {
             showSleepActivity();
@@ -70,12 +106,50 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
+  
+    
     // Проверим зарегистрировано ли приложение
     // Показать активити регистрации
     public void showSleepActivity(){
         Intent intent = new Intent(this, SleepActivity.class);
         startActivity(intent);
     }
+
+
+    @Override
+    public void dataHandler(String str) {
+        Log.i("dataHandler", str);
+
+        // Есть подключение
+        if(str != null){
+
+            if(!connectTitle){
+                connectTitle = true;
+                setTitle(getString(R.string.app_name) + " - " + getString(R.string.connect_ok) );
+                getSupportActionBar().setBackgroundDrawable(
+                        new ColorDrawable( getResources().getColor(R.color.colorPowerOn) )
+                );
+            }
+
+
+        // Нет подключения
+        }else{
+
+            if(connectTitle){
+                connectTitle = false;
+                setTitle(getString(R.string.app_name) + " - " + getString(R.string.connect_no) );
+                getSupportActionBar().setBackgroundDrawable(
+                        new ColorDrawable( getResources().getColor(R.color.colorNoConnect) )
+                );
+            }
+
+        }
+
+
+    }
+
+
+
+
 
 }
